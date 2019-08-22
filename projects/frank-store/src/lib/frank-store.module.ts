ed1a6f01e06ff83aab2eb8fe0bd497e62ac6f268
Frank Anneveld <frank@wirelab.nl>
2019-08-23 00:26:36 +0200

    added feature frank

--- projects/frank-store/src/lib/frank-store.module.ts
+++ projects/frank-store/src/lib/frank-store.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FrankStoreComponent } from './frank-store.component';
+import {ActionReducerMap, StoreModule} from '@ngrx/store';
 
-
+export const reducers: ActionReducerMap<any> = {};
 
 @NgModule({
   declarations: [FrankStoreComponent],
   imports: [
+    StoreModule.forFeature('featureFrank', reducers)
   ],
   exports: [FrankStoreComponent]
 })
