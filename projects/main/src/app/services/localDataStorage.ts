067e5ecd95039e365073f059e0724a3e79432d7c
Frank Anneveld <frank@wirelab.nl>
2019-09-19 21:23:33 +0200

    Ensure subscription unsubscribed after emit promise

--- projects/main/src/app/services/localDataStorage.ts
+++ projects/main/src/app/services/localDataStorage.ts
@@ -8,7 +8,7 @@
 import { APP_INITIALIZER, Injectable } from '@angular/core';
 import { CachedItem, Driver, NgForage, NgForageCache, NgForageOptions } from 'ngforage';
 import { PlatformService } from './platform.service';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, take } from 'rxjs/operators';
 import { from, Observable, of } from 'rxjs';
 import * as uuid from 'uuid';
 
@@ -38,21 +38,21 @@ export class LocalDataStorage {
   }
 
   public getKeys(): Observable<string[]> {
-    return from(this.cache.keys());
+    return from(this.cache.keys()).pipe(take(1));
   }
 
   public getLength(): Observable<number> {
-    return from(this.cache.length());
+    return from(this.cache.length()).pipe(take(1));
   }
 
   public setCachedItem<T>(key: string, data: T, cacheTime?: number): Observable<T | null> {
-    return from(this.cache.setCached<T>(key, data, cacheTime));
+    return from(this.cache.setCached<T>(key, data, cacheTime)).pipe(take(1));
   }
 
   public getCachedItem<T = any>(key: string): Observable<CachedItem<T> | null> {
     return from(this.cache.getCached<T>(key)).pipe(
       map((r: any) => (!r.hasData || r.expired) ? null : r.data)
-    );
+    ).pipe(take(1));
   }
 
   factory(): Promise<boolean> {
